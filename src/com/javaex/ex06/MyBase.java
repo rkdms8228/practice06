91d99b58a0b1851558e93578b12f720fb276f74f
가은 <rkdms8228@naver.com>
2022-04-27 19:47:09 +0900

    package06

--- src/com/javaex/ex06/MyBase.java
+++ src/com/javaex/ex06/MyBase.java
@@ -2,6 +2,20 @@ package com.javaex.ex06;
 
 public class MyBase extends Base{
 
-    //코드작성
+	//필드
+	
+	//생성자
+	
+	//메소드-gs
+	
+	//메소드-일반
+	public void afternoon() {
+		System.out.println("오후");
+	}
+	
+	@Override
+	public String toString() {
+		return  super.getDay()+"에는 열심히 수업듣자"+"\n"+super.getNight()+"\n"+afternoon+"도 "+super.getDay()+"과 마찬가지로 공부해야 합니다.";
+	}
     
 }
