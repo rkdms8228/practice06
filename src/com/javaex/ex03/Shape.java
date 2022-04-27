40d42e476f6cb9712a086145655413d69437fdd8
가은 <rkdms8228@naver.com>
2022-04-27 18:51:42 +0900

    package03

--- src/com/javaex/ex03/Shape.java
+++ src/com/javaex/ex03/Shape.java
@@ -3,13 +3,28 @@ package com.javaex.ex03;
 public class Shape {
 	
 	//필드
-	
+	protected String fillColor;
+	protected String lineColor;
 	
 	//생성자
-	
+
 	
 	//메소드-gs
-	
+	public String getFillColor() {
+		return fillColor;
+	}
+
+	public void setFillColor(String fillColor) {
+		this.fillColor = fillColor;
+	}
+
+	public String getLineColor() {
+		return lineColor;
+	}
+
+	public void setLineColor(String lineColor) {
+		this.lineColor = lineColor;
+	}
 	
 	//메소드-일반
 	
