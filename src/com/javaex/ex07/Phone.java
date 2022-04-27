34d480e36ab7bc68d6840da916bd8ed04cc80a27
가은 <rkdms8228@naver.com>
2022-04-27 21:42:07 +0900

    package07

--- src/com/javaex/ex07/Phone.java
+++ src/com/javaex/ex07/Phone.java
@@ -2,11 +2,11 @@ package com.javaex.ex07;
 
 public class Phone {
 
-    public void execute(String str){
-        call();
-    }
-    
-    private void call(){
-        System.out.println("통화기능시작");
-    }
+	public void execute(String str) {
+		call();
+	}
+
+	private void call() {
+		System.out.println("통화기능시작");
+	}
 }
--- src/com/javaex/ex07/SmartPhone.java
+++ src/com/javaex/ex07/SmartPhone.java
@@ -1,17 +1,19 @@
 package com.javaex.ex07;
 
 public class SmartPhone extends MusicPhone {
-    
-    public void execute(String str){
-        
-        //코드작성
-        
-    }
- 
-    //메소드작성
-    
-    //메소드작성
-    
-    
-    
+
+	public void execute(String str) {
+
+		//코드작성
+		super.execute(str);
+
+	}
+
+	//메소드작성
+	protected void app() {
+		System.out.println("앱실행");
+	}
+
+	//메소드작성
+
 }
